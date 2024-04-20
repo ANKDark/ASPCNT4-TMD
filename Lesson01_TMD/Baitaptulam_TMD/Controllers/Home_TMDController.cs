3da444def802065af3b0ed29f7d3a5672df96b23
ANKDark <dinhhoangducdbp2004@gmail.com>
2024-04-20 12:47:28 +0700

    Day01-Lesson01

---
+++ Lesson01_TMD/Baitaptulam_TMD/Controllers/Home_TMDController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baitaptulam_TMD.Controllers
+{
+    public class Home_TMDController : Controller
+    {
+        // GET: Home_TMD
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Đại học Nguyễn Trãi";
+            return View();
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
