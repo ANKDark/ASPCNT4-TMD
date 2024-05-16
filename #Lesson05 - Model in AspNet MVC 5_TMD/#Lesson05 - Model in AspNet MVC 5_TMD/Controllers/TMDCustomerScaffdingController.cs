50105cbd352096c913e631d04be5b38fd93340db
ANKDark <dinhhoangducdbp2004@gmail.com>
2024-05-16 12:21:48 +0700

    Lesson05

--- #Lesson05 - Model in AspNet MVC 5_TMD/#Lesson05 - Model in AspNet MVC 5_TMD/Controllers/TMDCustomerScaffdingController.cs
+++ #Lesson05 - Model in AspNet MVC 5_TMD/#Lesson05 - Model in AspNet MVC 5_TMD/Controllers/TMDCustomerScaffdingController.cs
@@ -7,33 +7,10 @@ using System.Web.Mvc;
 
 namespace _Lesson05___Model_in_AspNet_MVC_5_TMD.Controllers
 {
-    public class TMDCustomerController : Controller
+    public class TMDCustomerScaffdingController : Controller
     {
-        // GET: TMDCustomer
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult TMDCustomerDetails()
-        {
-            // Tạo đối tượng dữ liệu
-            var customer_tmd = new TMDCustomer()
-            {
-                CustomerID_TMD = 1,
-                FirstName_TMD = "Trần Minh",
-                LastName_TMD = " Đức",
-                Address_TMD = "Hà Nội",
-                YearOfBirth_TMD = 2004,
-        };
-            ViewBag.customer_tmd = customer_tmd;
-            return View();
-        }
-
-        //GET: TMDListCustomer
-        public ActionResult TMDListCustomer()
-        {
-            var list_tmd = new List<TMDCustomer>()
+        //mockdata
+        private static List<TMDCustomer> listCustomer_TMD = new List<TMDCustomer>()
             {
                 new TMDCustomer
                 {
@@ -76,8 +53,54 @@ namespace _Lesson05___Model_in_AspNet_MVC_5_TMD.Controllers
                     YearOfBirth_TMD = 2004,
                 },
             };
-            //ViewBag.list_tmd = list_tmd;
-            return View(list_tmd);
+
+
+        // GET: TMDCustomerScaffding
+        //List Customer
+        public ActionResult Index()
+                {
+                    return View(listCustomer_TMD);
+                }
+        [HttpGet]
+        public ActionResult Create_TMD()
+        {
+            var model_tmd = new TMDCustomer();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create_TMD(TMDCustomer model)
+        {
+            //Thêm đối tượng khách hàng vào ds dữ liệu
+            listCustomer_TMD.Add(model);
+            //return View();
+            //Chuyển về trang ds
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit_TMD(int id)
+        {
+            var customer_tmd = listCustomer_TMD.FirstOrDefault(x => x.CustomerID_TMD == id);
+            if (customer_tmd == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_tmd);
+        }
+
+        public ActionResult Details_TMD(int id)
+        {
+            var customer_tmd = listCustomer_TMD.FirstOrDefault(x => x.CustomerID_TMD == id);
+            if (customer_tmd == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_tmd);
+        }
+        public ActionResult Delete_TMD(int? id)
+        {
+            var customer_tmd = listCustomer_TMD.FirstOrDefault(x => x.CustomerID_TMD == id);
+            listCustomer_TMD.Remove(customer_tmd);
+            return RedirectToAction("Index");
         }
     }
 }
