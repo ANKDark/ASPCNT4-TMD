55d17b82528941cc30648c480a337ae3e4cc8c6f
ANKDark <dinhhoangducdbp2004@gmail.com>
2024-06-05 17:27:40 +0700

    Lesson06

---
+++ TmdLesson06CF/TmdLesson06CF/Controllers/TmdCategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TmdLesson06CF.Models;
+
+namespace TmdLesson06CF.Controllers
+{
+    public class TmdCategoriesController : Controller
+    {
+        private static TmdBookStore tmdDb;
+        public TmdCategoriesController()
+        {
+            tmdDb = new TmdBookStore();
+        }
+        // GET: TmdCategories
+        public ActionResult TmdIndex()
+        {
+            /*
+             * Khởi tạo DBcontext;
+             * EF sẽ tìm thông tin kết nối trong file machinee.config của .Net Famework trên máy
+             * Và sau đó tạo csdl
+             * */
+            var tmdCategory = tmdDb.TmdCategories.ToList();
+            return View(tmdCategory);
+        }
+        public ActionResult TmdCreate()
+        {
+            var tmdCategory = new TmdCategory();
+            return View(tmdCategory);
+        }
+        [HttpPost]
+        public ActionResult TmdCreate(TmdCategory tmdCategory)
+        {
+            tmdDb.TmdCategories.Add(tmdCategory);
+            tmdDb.SaveChanges();
+            return RedirectToAction("TmdIndex");
+        }
+    }
+}
---
+++ TmdLesson06CF/TmdLesson06CF/Models/TmdBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TmdLesson06CF.Models
+{
+    public class TmdBook
+    {
+        [Key]
+        public int TmdId { get; set; }
+        public string TmdBookId { get; set; }
+        public string TmdTitle { get; set; }
+        public string TmdAuthor { get; set;}
+        public int TmdYear { get; set;}
+        public string TmdPublic { get; set;}
+        public string TmdPicture { get; set;}
+        public int TmdCategoryId { get; set;}
+        // Thuộc tính quan hệ
+        public virtual TmdCategory TmdCategory { get; set;}
+    }
+}
