6636907c449dd2dbe6171e51782bdee8203e7143
ANK Dark <dinhhoangducdbp2004@gmail.com>
2024-07-04 10:21:15 +0700

    Lesson11

---
+++ TmdLesson11Db-2210900014/TmdLesson11Db-2210900014/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TmdLesson11Db_2210900014
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TmdLesson11Db-2210900014/TmdLesson11Db-2210900014/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TmdLesson11Db_2210900014.MvcApplication" Language="C#" %>
